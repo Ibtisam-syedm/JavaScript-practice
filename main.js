38a2f640031832ed68a9d2a5969c4092e378cc34
Ibtisam <m.ibtisam.syed@gmail.com>
2022-07-08 15:29:20 +0500

    added old functions of conversion with obejects

--- main.js
+++ main.js
@@ -4,7 +4,13 @@ let user={
     [Symbol.toPrimitive](hint){
         alert(`hint : ${hint}`);
         return hint === "string" ? `name : "${this.name}" ` : this.money;
+    },
+    toString(){
+        return this.name;
+    },
+    valueOf(){
+        return this.money;
     }
 }
 
-alert(user);
+alert(+user);
