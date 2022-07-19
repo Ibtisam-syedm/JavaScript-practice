b3a8946e2da7f3d649e5fe43cead24f6b457d92f
Ibtisam <m.ibtisam.syed@gmail.com>
2022-07-19 17:26:47 +0500

    examples

--- numbers.js
+++
@@ -1,9 +0,0 @@
-function getMaxSubSum(arr){
-    let tempSum = 0, maxSum = 0;
-    for(let value of arr){
-        tempSum += value;
-        maxSum = Math.max(maxSum,tempSum);
-        if (tempSum < 0) tempSum = 0;
-    }
-    return maxSum;
-}
