f396041b5012456527cda3af9689f450bd48aa7b
Ibtisam <m.ibtisam.syed@gmail.com>
2022-07-23 16:21:03 +0500

    learned basic math operations

---
+++ basicMathoperations.js
@@ -0,0 +1,30 @@
+alert(2 % 2)//0
+alert(2 ** 3)//8
+
+//for square root
+alert(2 ** (1/2));//1.414...
+
+let s = "my" + "string";
+alert(s);//mystring
+
+alert(3 + 3 + "3");//63
+
+alert("6" - "3");//3
+alert(1 - "3");//-2
+alert(+"2" + +"o")//6
+let a = 1;
+let b = 1;
+let c = 4 - (a = b + 2);
+alert(a)//3
+alert(c)//1
+let m = 2;
+m *= 4 + 4;// m = m * (4 + 4)
+alert(m);
+
+alert("" + 1 + 0);//10
+alert("" - 1 + 0);//-1
+alert(true + false);//1
+alert(6 / "3");//2
+alert("2" * "3");//6
+alert(4 + 5 + "px");//9px
+alert("     " -2 );//-2
